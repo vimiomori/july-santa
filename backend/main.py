9d09f522d54470276fa7eb2e184b44871dcf6969
vimiomori <vivian.muchen@gmail.com>
2023-07-15 16:39:47 +0900

    ad cors

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
+import pickle
 import uvicorn
 import os
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
