f78a59e140d022e84b8083ae716bd8ace5f9b3f9
vimiomori <vivian.muchen@gmail.com>
2023-07-15 17:44:27 +0900

    add joke api

--- backend/main.py
+++ backend/main.py
@@ -53,9 +53,13 @@ def read_present(name):
         "imageURL": imageURL
     }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/joke")
+def joke():
+    jokes = CONFIG["no"]
+    return {
+        "joke": jokes[randrange(len(jokes))]
+    }
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ['PORT']))
