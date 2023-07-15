a13fa2164b8a6a70132567a438a116e7679dc411
vimiomori <vivian.muchen@gmail.com>
2023-07-15 15:36:09 +0900

    remove dockerfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,3 +13,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
