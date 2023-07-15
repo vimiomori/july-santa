ee9bbdad4684113a4721cedf6ea3676821ed45b6
scoffrin <scoffrin@indeed.com>
2023-07-15 14:31:15 +0900

    santa image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import santabeach from './assets/santa-beach.jpeg'
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <div id="App">
       <h1>santa in july</h1>
+      <img src={santabeach} alt="santa literally on the beach" />
     </div>
   )
 }
